e91c5612752ae561b27a2240edfbc39074ece21a
KimYeaSeul <adgjl3151@hanmail.net>
2022-07-11 19:41:03 +0900

    fix : template -> templates

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 from pymongo import MongoClient
@@ -6,9 +8,9 @@ ca = certifi.where()
 client = MongoClient('url', tlsCAFile=ca)
 db = client.dbsparta
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/", methods=["POST"])
 def _post():
@@ -22,10 +24,6 @@ def bucket_done():
     print(sample_receive)
 
     return jsonify({'msg': 'POST'})
-
-@app.route("/", methods=["GET"])
-def bucket_get():
-    return jsonify({'sample': sample_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
