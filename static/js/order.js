3d8a5271f3acfa77784a759ba4d1248c118b2ce0
kwonih1020 <kwonih1020@gmail.com>
2022-07-12 21:22:13 +0900

    comments

--- static/js/order.js
+++ static/js/order.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
     showList()
 })
 
@@ -12,14 +12,13 @@ function showList() {
         let place = placeHtml[i]
         let menu = menuHtml[i]
 
-        if (place==null) {
+        if (place == null) {
             place = ``
         }
-        if (menu==null) {
+        if (menu == null) {
             menu = ``
         }
         html.push([place, menu])
-        
     }
 
     for (let i = 0; i < html.length; i++) {
@@ -34,7 +33,6 @@ function showList() {
                         </tr>`
         $('#menuList').append(temp_html);
     }
-    
 }
 
 function getPlace() {
@@ -52,22 +50,19 @@ function getPlace() {
                 let placeAddress = element['storeAddress']
                 let placeImage = 'static/images/place_image.jpeg'
 
-                let temp_html = `
-                            <div class="card mb-3">
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                        <img src=${placeImage} class="img-fluid rounded-start" alt="...">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${placeName}</h5>
-                                            <p class="card-text">${placeAddress}</p>
+                let temp_html = `<div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src=${placeImage} class="img-fluid rounded-start" alt="...">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${placeName}</h5>
+                                                <p class="card-text">${placeAddress}</p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            `
-                
+                                </div>`
                 placeHtml.push(temp_html)
             }
         }
@@ -90,24 +85,21 @@ function getMenu() {
                 let menuImage = element['image']
                 let menuCost = element['cost']
 
-                let temp_html = `
-                            <div class="card mb-3"">
-                                <div class="row g-0">
-                                        <input class="form-check-input mt-0" type="checkbox" value='${JSON.stringify(element)}' aria-label="Checkbox for following text input" name="menuCheckbox">
-                                    <div class="col-md-4">
-                                        <img src="${menuImage}" class="img-fluid rounded-start" alt="...">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${menuName}</h5>
-                                            <p class="card-text">${menuCost}</p>
+                let temp_html = `<div class="card mb-3"">
+                                    <div class="row g-0">
+                                            <input class="form-check-input mt-0" type="checkbox" value='${JSON.stringify(element)}' aria-label="Checkbox for following text input" name="menuCheckbox">
+                                        <div class="col-md-4">
+                                            <img src="${menuImage}" class="img-fluid rounded-start" alt="...">
                                         </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${menuName}</h5>
+                                                <p class="card-text">${menuCost}</p>
+                                            </div>
+                                        </div>
+                                        <input type="number" id="quantity" name="count" min="0" max="100">
                                     </div>
-                                    <input type="number" id="quantity" name="count" min="0" max="100">
-                                </div>
-                            </div>
-                            `
-                
+                                </div>`
                 menuHtml.push(temp_html)
             }
         }
@@ -118,11 +110,11 @@ function getMenu() {
 function orderComplete() {
     let orders = []
     $('input:checkbox[name="menuCheckbox"]').each(function () {
-        if(this.checked){
+        if (this.checked) {
             console.log(this.value)
             orders.push(JSON.parse(this.value,))
         }
-      })
+    })
     console.log(orders)
     // location.href = '/pay'
 }
