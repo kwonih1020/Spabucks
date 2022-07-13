d610df954966c3f3792364fe5197ed806217a63f
Antony Koo <kina5418@naver.com>
2022-07-13 11:44:04 +0900

    회원가입/로그인 기능 완료

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
+import jwt
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
 import hashlib
 import json
-<<<<<<< HEAD
 
 app = Flask(__name__)
 
-ca = certifi.where()
-SECRET_KEY = 'SPARTA'  # 숨겨 주세요.
-client = MongoClient('mongodb+srv://junior_koo:test@cluster0.ty9x0.mongodb.net/?retryWrites=true&w=majority')
+SECRET_KEY =
 
-app = Flask(__name__)
 ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.axu42.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient()
 db = client.spabucks
 
 
@@ -26,8 +22,8 @@ def home():
 
 @app.route('/sign_up/check_id', methods=['POST'])
 def check_id():
-    username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
+    user_id_receive = request.form['userId_give']
+    exists = bool(db.users.find_one({"userId": user_id_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 
@@ -40,18 +36,40 @@ def check_nickname():
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
-    username_receive = request.form['username_give']
+    userid_receive = request.form['userId_give']
     password_receive = request.form['password_give']
     nickname_receive = request.form['nickname_give']
 
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,  # 아이디
+        "userId": userid_receive,  # 아이디
         "password": password_hash,  # 비밀번호
         "nickname": nickname_receive,  # 닉네임
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    userid_receive = request.form['userId_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'userId': userid_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': userid_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 @app.route("/order", methods=["GET"])
